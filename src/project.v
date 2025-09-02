50ec507af1c729f010ac01032c9bc4c2349e8da7
Patrick Schaumont <pschaumont@wpi.edu>
2025-09-02 12:23:25 -0400

    update

--- src/project.v
+++ src/project.v
@@ -32,15 +32,15 @@ module tt_um_delaychain (
    genvar i;
    generate
       for (i = 0; i < 8; i = i + 1) begin : thechain
-	 testchain #(.P(1))   chain1  (.clk(clk), .rst_n(rst)n), .din(ui_in[i]), .test(uio_in[0]), .dout(c1q[i]));
-	 testchain #(.P(2))   chain2  (.clk(clk), .rst_n(rst)n), .din(  c1q[i]), .test(uio_in[0]), .dout(c2q[i]));
-	 testchain #(.P(5))   chain5  (.clk(clk), .rst_n(rst)n), .din(  c2q[i]), .test(uio_in[0]), .dout(c5q[i]));
-	 testchain #(.P(10))  chain10 (.clk(clk), .rst_n(rst)n), .din(  c5q[i]), .test(uio_in[0]), .dout(c10q[i]));
-	 testchain #(.P(20))  chain20 (.clk(clk), .rst_n(rst)n), .din( c10q[i]), .test(uio_in[0]), .dout(c20q[i]));
-	 testchain #(.P(50))  chain50 (.clk(clk), .rst_n(rst)n), .din( c20q[i]), .test(uio_in[0]), .dout(c50q[i]));
-	 testchain #(.P(100)) chain100(.clk(clk), .rst_n(rst)n), .din( c50q[i]), .test(uio_in[0]), .dout(c100q[i]));
-	 testchain #(.P(200)) chain200(.clk(clk), .rst_n(rst)n), .din(c100q[i]), .test(uio_in[0]), .dout(c200q[i]));
-	 testchain #(.P(500)) chain500(.clk(clk), .rst_n(rst)n), .din(c200q[i]), .test(uio_in[0]), .dout(c500q[i]));
+	 testchain #(.P(1))   chain1  (.clk(clk), .rst_n(rst_n), .din(ui_in[i]), .test(uio_in[0]), .dout(c1q[i]));
+	 testchain #(.P(2))   chain2  (.clk(clk), .rst_n(rst_n), .din(  c1q[i]), .test(uio_in[0]), .dout(c2q[i]));
+	 testchain #(.P(5))   chain5  (.clk(clk), .rst_n(rst_n), .din(  c2q[i]), .test(uio_in[0]), .dout(c5q[i]));
+	 testchain #(.P(10))  chain10 (.clk(clk), .rst_n(rst_n), .din(  c5q[i]), .test(uio_in[0]), .dout(c10q[i]));
+	 testchain #(.P(20))  chain20 (.clk(clk), .rst_n(rst_n), .din( c10q[i]), .test(uio_in[0]), .dout(c20q[i]));
+	 testchain #(.P(50))  chain50 (.clk(clk), .rst_n(rst_n), .din( c20q[i]), .test(uio_in[0]), .dout(c50q[i]));
+	 testchain #(.P(100)) chain100(.clk(clk), .rst_n(rst_n), .din( c50q[i]), .test(uio_in[0]), .dout(c100q[i]));
+	 testchain #(.P(200)) chain200(.clk(clk), .rst_n(rst_n), .din(c100q[i]), .test(uio_in[0]), .dout(c200q[i]));
+	 testchain #(.P(500)) chain500(.clk(clk), .rst_n(rst_n), .din(c200q[i]), .test(uio_in[0]), .dout(c500q[i]));
 	 assign uo_out[i] = c500q[i];
       end
    endgenerate
